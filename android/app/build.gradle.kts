0cc1b9ba6f3e63da7a4e6280ada9e4dc54a0b087
cmachsocket <cmach_socket@outlook.com>
2026-09-21 03:58:08 +0800

    fix syntax

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,8 @@ android {
     namespace = "com.cmach.oceanus"
     compileSdk = flutter.compileSdkVersion
 
-    // 强制覆盖 Flutter SDK 写死的 ndkVersion=28.2.13676358,
-    // 用系统里已有的 29.0.14206865(避免自动下载/无写权限)。
-    ndkVersion = 30.0.16248370
+
+    ndkVersion = "30.0.16248370"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
